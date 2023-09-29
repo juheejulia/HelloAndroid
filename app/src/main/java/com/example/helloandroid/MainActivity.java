1efa84bed44be6e0964cff2951d944790f73f1ad
Juhee Kang <juhee.kang@infotiv.se>
2023-09-30 01:17:51 +0200

    Fix different screen size.

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -4,55 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    //run 'my_profile.xml'
-    TextView textView;
-    Button helloButton;
-    // Button helloButton = findViewById(R.id.btn_hello); instead helloButton = findViewById(R.id.btn_hello); under onCreate()
+    ImageView profilePicture;
+    TextView myFullName;
+    TextView jobRole;
+    Button cvButton;
+    Button portfolio;
+    TextView presentation;
+    ImageButton linkedinButton;
+    ImageButton emailButton;
+    ImageButton mobileButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_profile);
 
+        profilePicture = findViewById(R.id.profile_picture);
+        myFullName = findViewById(R.id.full_name);
+        jobRole = findViewById(R.id.job_role);
+        portfolio = findViewById(R.id.btn_portfolio);
+        presentation = findViewById(R.id.presentation);
+        linkedinButton = findViewById(R.id.btn_linkedin);
+        emailButton = findViewById(R.id.btn_email);
+        mobileButton = findViewById(R.id.btn_mobile);
 
-
-    //run 'activity_main.xml'
-    /*
-    TextView textView;
-    Button helloButton;
-    // Button helloButton = findViewById(R.id.btn_hello); instead helloButton = findViewById(R.id.btn_hello); under onCreate()
-    EditText inputText;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //setContentView(android.layout.activity_main);
-
-        textView = findViewById(R.id.tv_greeting);
-        inputText = findViewById(R.id.message);
-        textView.setText(R.string.hello_text);
-
-        helloButton = findViewById(R.id.btn_hello);
-        helloButton.setOnClickListener(new View.OnClickListener() {
+        cvButton = findViewById(R.id.btn_cv);
+        cvButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View button){
-            Toast.makeText(MainActivity.this,R.string.toast_text,Toast.LENGTH_LONG).show();
-            getTextMessage();
+            public void onClick(View view) {
+                simulateButtonClick();
+            }
+            public void simulateButtonClick() {
+                cvButton.performClick();
+            }
+            public void getTextMessage() {
             }
         });
-    }
-    public void getTextMessage() {
-        String message = inputText.getText().toString();
-        textView.setText(message);
-    }
-    */
-
     }
 }
